a53247233128bc176d00c1c6b682983dd32f76d4
smlevine <SMLevine@my.harrisburgu.edu>
2016-11-08 15:27:19 -0500

    Add files via upload
    Assignment for Bubble Sorting

---
+++ Bubble Sorting Assingment.py
@@ -0,0 +1,15 @@
+def bubbleSort(alist):
+    changetoshorten = True
+    passnum = len(alist)-1
+    while passnum > 0 and changetoshorten:
+        changetoshorten = False
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+bubbleSort(alist)
+print(alist)
